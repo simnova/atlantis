7d50fc8f8aee9f6922005d9c697927e47a3a69db
Patrick Gidich <patrick.gidich@simnova.com>
2022-07-24 22:12:21 -0400

    animations

--- src/components/layouts/root/pages/home.tsx
+++ src/components/layouts/root/pages/home.tsx
@@ -5,6 +5,8 @@ import "swiper/css/navigation";
 
 import { Autoplay, Pagination, Navigation } from "swiper";
 import { Helmet } from 'react-helmet';
+import { motion, useAnimation } from 'framer-motion';
+import { HashLink as Link } from 'react-router-hash-link';
 
 /*
 
@@ -17,11 +19,15 @@ https://www.responsivebreakpoints.com/
 */
 
 export const Home: React.FC<any> = (_props) => {
+  const boxVariant = {
+    visible: { opacity: 1, scale: 2 },
+    hidden: { opacity: 0, scale: 0 },
+  }
+
   return (<>
     <Helmet>
       <title>Welcome</title>
     </Helmet>
-    <h1>Welcome to the Atlantis in Ocean City, MD</h1>
     <article >
         <Swiper
            spaceBetween={30}
@@ -119,5 +125,142 @@ export const Home: React.FC<any> = (_props) => {
         </SwiperSlide>
       </Swiper>
     </article>
+
+    <article className='space-y-9 mt-12'>
+      <div className='relative'>
+        <motion.div
+          
+          variants={boxVariant}
+          animate={{x:-100, opacity: 0 }}
+          whileInView={{x:0, opacity: 1 }}
+          viewport={{ once: false }}
+          className="md:absolute"
+          transition={{ ease: "easeOut", duration: 1 }}
+        >
+          <div className="p-8 bg-spicy-mix-50/90 w-100 md:w-[350px]" style={{height:'40vh', minHeight:'400px', minWidth:'300px'}}>
+            <div className='flex flex-row items-center'>
+              <div style={{fontFamily: 'Black Chancery',fontSize:'80px'}} className="text-blue-900/10">A</div>
+              <div className='-ml-8 text-blue-900 font-light text-4xl'>AMENITIES</div>
+            </div>
+            Pool / Beach / Proximity to Golfing <br/>
+            Learn all about what Atlantis has to Offer.
+
+            <div className='py-4 mt-5 space-x-2'>
+              <Link className="py-2 px-4 bg-blue-900 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" to='/visiting/amenities#'>Learn More</Link>
+            </div>
+          </div>
+        </motion.div>
+      
+        <picture>
+          <img
+            sizes="(max-width: 1400px) 100vw, 1400px"
+            srcSet="
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_200.jpg 200w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_455.jpg 455w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_627.jpg 627w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_653.jpg 653w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_992.jpg 992w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_964.jpg 964w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_1189.jpg 1189w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_1277.jpg 1277w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_1354.jpg 1354w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_1364.jpg 1364w,
+              /assets/carousel/carousel-3-min_u18n1y_c_scale,w_1400.jpg 1400w"
+            src="carousel-3-min_u18n1y_c_scale,w_1400.jpg"
+            alt=""/>
+          </picture>
+
+      </div>
+      <div className='relative'>
+        <motion.div
+          
+          variants={boxVariant}
+          animate={{x:-100, opacity: 0 }}
+          whileInView={{x:0, opacity: 1 }}
+          viewport={{ once: false }}
+          className="md:absolute"
+          transition={{ ease: "easeOut", duration: 1 }}
+        >
+          <div className="p-8 bg-spicy-mix-100/90 h-100 w-100 md:w-[350px]"  style={{   minWidth:'300px'}}>
+            <div className='flex flex-row items-center'>
+              <div style={{fontFamily: 'Black Chancery',fontSize:'80px'}} className="text-blue-900/10">E</div>
+              <div className='-ml-8  text-blue-900 font-light text-4xl'>EXPLORE</div>
+            </div>
+            Atlantis is located directly on the beach and close to everything you need to enjoy your stay.<br/>
+            <div className="grid mt-5" style={{ gridTemplateColumns:'repeat(auto-fill,120px)', gap:'5px'}}>
+              <div className='inline-block'><Link className="block w-[120px] py-2 px-4 bg-blue-900 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" to='/explore/dining#'>Dining</Link></div>
+              <div className='inline-block'><Link className="block w-[120px] py-2 px-4 bg-blue-900 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" to='/explore/activities#'>Activities</Link></div>
+              <div className='inline-block'><Link className="block w-[120px] py-2 px-4 bg-blue-900 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" to='/explore/attractions#'>Attractions</Link></div>
+              <div className='inline-block'><Link className="block w-[120px] py-2 px-4 bg-blue-900 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" to='/explore/events#'>Events</Link></div>
+            </div>
+          </div>
+        </motion.div>
+        <picture>
+          <img
+            sizes="(max-width: 1400px) 100vw, 1400px"
+            srcSet="
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_200.jpg 200w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_482.jpg 482w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_674.jpg 674w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_842.jpg 842w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_970.jpg 970w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_1101.jpg 1101w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_1219.jpg 1219w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_1396.jpg 1396w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_1347.jpg 1347w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_1394.jpg 1394w,
+              /assets/carousel/carousel-4-min_gk4c7m_c_scale,w_1400.jpg 1400w"
+              src="/assets/carousel/carousel-4-min_gk4c7m_c_scale,w_1400.jpg"
+            alt="" />
+          </picture>
+      </div>
+      <div className='relative'>
+
+        <motion.div
+          
+          variants={boxVariant}
+          animate={{x:-100, opacity: 0 }}
+          whileInView={{x:0, opacity: 1 }}
+          viewport={{ once: false }}
+          className="md:absolute"
+          transition={{ ease: "easeOut", duration: 1 }}
+        >
+          <div className="p-8 bg-spicy-mix-200/90 w-100 md:w-[350px]"  style={{height:'40vh',minHeight:'400px',  minWidth:'300px'}}>
+          <div className='flex flex-row items-center'>
+              <div style={{fontFamily: 'Black Chancery',fontSize:'80px'}} className="text-blue-900/10">O</div>
+              <div className='-ml-8  text-blue-900 font-light text-4xl'>OWNERSHIP</div>
+            </div>
+            Get to know the benefits of ownership at the Atlantis.<br/>
+
+            <div className='py-4 mt-5 space-x-2'>
+              <Link className="py-2 px-4 bg-blue-900 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" to='/about/ownership#'>Learn More</Link>
+            </div>
+          </div>
+        </motion.div>
+        <picture>
+          <img
+            sizes="(max-width: 1400px) 100vw, 1400px"
+            srcSet="
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_200.jpg 200w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_400.jpg 400w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_570.jpg 570w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_703.jpg 703w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_817.jpg 817w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_917.jpg 917w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_1005.jpg 1005w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_1099.jpg 1099w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_1188.jpg 1188w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_1270.jpg 1270w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_1355.jpg 1355w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_1389.jpg 1389w,
+              /assets/carousel/carousel-2-min_oxxtmu_c_scale,w_1400.jpg 1400w"
+            src="carousel-2-min_oxxtmu_c_scale,w_1400.jpg"
+            alt=""/>
+        </picture>
+      </div>
+
+    </article>
+    
+
   </>)
 }
