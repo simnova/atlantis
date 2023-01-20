6ecb35c95a20145ae53041cc8a870cd45fa22228
Patrick Gidich <patrick.gidich@simnova.com>
2023-01-20 17:35:47 -0500

    tweak

--- src/components/layouts/root/pages/contact.tsx
+++ src/components/layouts/root/pages/contact.tsx
@@ -25,30 +25,31 @@ export const Contact: React.FC<any> = (_props) => {
 
         <article>
           <Heading level={HeadingLevel.H5}>Office Staff:</Heading>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div className="grid grid-cols-1">
 
             <div className="flex items-center inline-block">
-              <img src={MelvinRostek.src}  srcSet={MelvinRostek.srcSet}  alt={`Profile of Melvin Rostek`} className="w-20 self-center rounded-full mt-6 ml-6 mb-6"/>
+              <img src="https://via.placeholder.com/200"  alt={`Profile of Melvin Rostek`} className="w-20 self-center rounded-full mt-6 ml-6 mb-6"/>
               <div className='ml-6'>
                 <strong>Kristin Moore</strong><br/>
                 <span>Community Association Manager</span>
               </div>
+              <div className='ml-3 mt-3'>
+                Kristin Moore is a highly experienced community association manager with over 15 years of experience in the field. 
+                She has managed a variety of properties, including townhomes, single family homes, and condominiums. 
+                She has a proven track record of preparing monthly management reports, including financials and delinquency reports, 
+                and overseeing the preparation and tracking of annual budgets. Kristin is skilled in Microsoft Suite, Jenark, Strongroom, 
+                and other industry operating systems. 
+                She is known for her excellent customer service skills and attention to detail, as well as her strong written and verbal 
+                communication skills, including contract negotiation. She is also proficient in human resources having managed a staff 
+                of 20+, vendor management, and risk management. 
+                Kristin holds a CMCA certification and an AMS certification. 
+                She is also very involved with CAI (Community Associations Institute), the group that governs the standards in the 
+                Community Association Management industry and currently holds two of the three certifications offered. 
+                She has also taken the necessary classes and exams to gain her CMCA (Certified Manager of Community Associations) and 
+                AMS (Association Management Specialist) certifications
+              </div>
             </div>
-            <div>
-              Kristin Moore is a highly experienced community association manager with over 15 years of experience in the field. 
-              She has managed a variety of properties, including townhomes, single family homes, and condominiums. 
-              She has a proven track record of preparing monthly management reports, including financials and delinquency reports, 
-              and overseeing the preparation and tracking of annual budgets. Kristin is skilled in Microsoft Suite, Jenark, Strongroom, 
-              and other industry operating systems. 
-              She is known for her excellent customer service skills and attention to detail, as well as her strong written and verbal 
-              communication skills, including contract negotiation. She is also proficient in human resources having managed a staff 
-              of 20+, vendor management, and risk management. 
-              Kristin holds a CMCA certification and an AMS certification. 
-              She is also very involved with CAI (Community Associations Institute), the group that governs the standards in the 
-              Community Association Management industry and currently holds two of the three certifications offered. 
-              She has also taken the necessary classes and exams to gain her CMCA (Certified Manager of Community Associations) and 
-              AMS (Association Management Specialist) certifications
-            </div>
+            
 
             
 
--- src/components/layouts/root/pages/visiting-notices.tsx
+++ src/components/layouts/root/pages/visiting-notices.tsx
@@ -36,20 +36,18 @@ export const VisitingNotices: React.FC<any> = (_props) => {
         <Heading level={HeadingLevel.H1} className="mt-6">Notices</Heading>
 
 
-        <div className="space-y-5">
+        <div className="space-y-5 mb-3">
           <article>
-            <Heading level={HeadingLevel.H5}>
-            
-              JAN-2022 - Welcome Kristin!</Heading>
-              <span className="px-3 py-1 mr-2 rounded-full font-semibold text-sm text-white bg-slate-500">Staffing</span><br/><br/>
+            <Heading level={HeadingLevel.H5}>JAN-2022</Heading>
+              <span className="px-3 py-1 mr-2 rounded-full font-semibold text-sm text-white bg-slate-500">Staffing</span> Welcome Kristin<br/><br/>
             <p>
  
               We are thrilled to announce that Kristin Moore has joined our community as the new Community Association Manager.<br/>
-              With over 15 years of experience in the field, Kristin brings a wealth of knowledge and expertise to our team.<br/>
+              With over 15 years of experience in the field, Kristin brings a wealth of knowledge and expertise to our team.
               She has managed a variety of properties, including townhomes, single family homes, and condominiums, and has a 
-              proven track record of preparing monthly management reports and overseeing the preparation and tracking of annual budgets.<br/>
+              proven track record of preparing monthly management reports and overseeing the preparation and tracking of annual budgets.
               Kristin is skilled in Microsoft Suite, Jenark, Strongroom, and other industry operating systems, and is known for her 
-              excellent customer service skills and attention to detail.<br/>
+              excellent customer service skills and attention to detail.<br/><br/>
               We are confident that Kristin will be an asset to our community, and we look forward to working with her. 
               <br/><br/>
               Welcome to the team, Kristin!
@@ -60,7 +58,7 @@ export const VisitingNotices: React.FC<any> = (_props) => {
           </article>
         </div>
         
-        <div className="space-y-5">
+        <div className="space-y-5  mb-3">
           <article>
             <Heading level={HeadingLevel.H5}>JUL-2022 - Cart Policy Update:</Heading>
             <p>
